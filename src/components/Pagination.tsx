83a973701a43f5dfc789f4083f826f4f95543565
souravMitra02 <inf.souravmitra@gmail.com>
2025-09-03 12:50:35 +0600

    added Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+interface Props {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export default function Pagination({ currentPage, totalPages, onPageChange }: Props) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <div className="flex space-x-2 mt-4 justify-center">
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => onPageChange(page)}
+          className={`px-3 py-1 rounded ${
+            page === currentPage
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+          }`}
+        >
+          {page}
+        </button>
+      ))}
+    </div>
+  );
+}
